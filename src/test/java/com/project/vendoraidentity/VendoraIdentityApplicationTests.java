28208b1702880c4e5e8ea18609a2013c7e1a334d
Tushar Srivastava <tusharsri90@gmail.com>
2025-07-09 19:59:27 +0530

    initial commit

---
+++ src/test/java/com/project/vendoraidentity/VendoraIdentityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.vendoraidentity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendoraIdentityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
